57f9294abec9c4c66a4c8ee95db0da1c84e1f971
kongou-ae <y.matsumoto.ae@gmail.com>
2019-08-24 21:56:03 +0900

    fixed #6

--- Test-Azure.ps1
+++ Test-Azure.ps1
@@ -3,7 +3,7 @@ param (
     [switch]$backup,
     [switch]$disk,
     [switch]$compute,
-    [switch]$PassThru,
+    [switch]$json,
     [switch]$export
 )
 
@@ -28,34 +28,38 @@ $TestResult = $result.TestResult | Select-Object Describe, Context, Name, Result
 # ToDo: ファイルに書き出す処理を足す
 #$result | ConvertTo-Json 
 
-# カテゴリを抽出する
-$describes = ($TestResult | Select-Object Describe | Sort-Object -Property Describe -Unique).Describe
+if( $json -eq $true ){
+    $TestResult | ConvertTo-Json 
+} else {
+    # カテゴリを抽出する
+    $describes = ($TestResult | Select-Object Describe | Sort-Object -Property Describe -Unique).Describe
 
-# カテゴリごとに処理をループ
-$describes | ForEach-Object {
-    $describe = $_
-    Write-Output "-----------------------------------------------------"
-    Write-Output "$describe"
-    Write-Output "-----------------------------------------------------"
+    # カテゴリごとに処理をループ
+    $describes | ForEach-Object {
+        $describe = $_
+        Write-Output "-----------------------------------------------------"
+        Write-Output "$describe"
+        Write-Output "-----------------------------------------------------"
 
-    # カテゴリ内のテスト項目を抽出
-    $contexts = ($TestResult | Where-Object { $_.Describe -eq $describe } | Select-Object Context | Sort-Object -Property Context -Unique).Context
+        # カテゴリ内のテスト項目を抽出
+        $contexts = ($TestResult | Where-Object { $_.Describe -eq $describe } | Select-Object Context | Sort-Object -Property Context -Unique).Context
 
-    # テスト項目ごとに処理をループ
-    $contexts | ForEach-Object {
-        $context = $_ 
-        Write-Output "$context"
+        # テスト項目ごとに処理をループ
+        $contexts | ForEach-Object {
+            $context = $_ 
+            Write-Output "$context"
 
-        # 全件からカテゴリとテスト項目に該当するものを抽出してループ
-        $TestResult | Where-Object { $_.Describe -eq $describe -and $_.Context -eq $context } | ForEach-Object {
-            $a = $_.Result
-            $b = $_.Name
-            switch ($a) {
-                "Passed" {
-                    Out-Log "  $($a) $($b)" "Green"
-                }
-                "Failed" {
-                    Out-Log "  $($a) $($b)" "Red"
+            # 全件からカテゴリとテスト項目に該当するものを抽出してループ
+            $TestResult | Where-Object { $_.Describe -eq $describe -and $_.Context -eq $context } | ForEach-Object {
+                $a = $_.Result
+                $b = $_.Name
+                switch ($a) {
+                    "Passed" {
+                        Out-Log "  $($a) $($b)" "Green"
+                    }
+                    "Failed" {
+                        Out-Log "  $($a) $($b)" "Red"
+                    }
                 }
             }
         }
